13275f6cfe5c6165b7b20498e8e646b0bbc45300
urfavboy21 <jihadfisabilillah1154@gmail.com>
2023-12-24 13:08:10 +0700

    tugas UAS PEMWEB 1

---
+++ database/dbpemesanan.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2023 at 03:19 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbpemesanan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemesanan`
+--
+
+CREATE TABLE `pemesanan` (
+  `id_pemesanan` int(50) NOT NULL,
+  `tanggal_pemesanan` date NOT NULL,
+  `total_belanja` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pemesanan`
+--
+
+INSERT INTO `pemesanan` (`id_pemesanan`, `tanggal_pemesanan`, `total_belanja`) VALUES
+(43, '2023-12-07', 29000000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemesanan_produk`
+--
+
+CREATE TABLE `pemesanan_produk` (
+  `id_pemesanan_produk` int(50) NOT NULL,
+  `id_pemesanan` int(50) NOT NULL,
+  `id_menu` varchar(50) NOT NULL,
+  `jumlah` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pemesanan_produk`
+--
+
+INSERT INTO `pemesanan_produk` (`id_pemesanan_produk`, `id_pemesanan`, `id_menu`, `jumlah`) VALUES
+(7, 32, '9', 1),
+(8, 32, '11', 1),
+(9, 33, '16', 1),
+(10, 33, '6', 1),
+(11, 34, '13', 1),
+(12, 34, '8', 1),
+(13, 34, '9', 1),
+(14, 34, '17', 1),
+(15, 35, '9', 2),
+(16, 35, '14', 1),
+(17, 36, '8', 1),
+(18, 37, '13', 1),
+(19, 37, '16', 1),
+(20, 38, '8', 1),
+(21, 39, '9', 1),
+(22, 39, '16', 1),
+(23, 40, '10', 1),
+(24, 40, '14', 1),
+(25, 41, '17', 1),
+(26, 41, '10', 1),
+(27, 41, '9', 2),
+(28, 42, '9', 1),
+(29, 42, '14', 1),
+(30, 42, '7', 1),
+(31, 42, '17', 1),
+(32, 43, '21', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_menu` int(50) NOT NULL,
+  `nama_menu` varchar(50) NOT NULL,
+  `jenis_menu` varchar(50) NOT NULL,
+  `stok` int(50) NOT NULL,
+  `harga` int(50) NOT NULL,
+  `gambar` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id_menu`, `nama_menu`, `jenis_menu`, `stok`, `harga`, `gambar`) VALUES
+(21, 'Iphone 14 Pro Max', '', 6, 29000000, 'iphone14promax.jpg'),
+(22, 'Redmi Note 12', '', 5, 2099000, 'redminote12.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `nama_lengkap` varchar(25) NOT NULL,
+  `jenis_kelamin` varchar(25) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `alamat` varchar(25) NOT NULL,
+  `hp` varchar(25) NOT NULL,
+  `status` enum('admin','user','','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama_lengkap`, `jenis_kelamin`, `tanggal_lahir`, `alamat`, `hp`, `status`) VALUES
+(7, 'bagusabiel', 'pemweb1', 'bagus abil', 'Laki-Laki', '2003-11-21', 'BKM', '085707025130', 'admin'),
+(8, 'agusss111', 'agus123', 'Agus lapar buk', 'Laki-Laki', '2010-12-01', 'Jombang', '085854121356', 'user');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  ADD PRIMARY KEY (`id_pemesanan`);
+
+--
+-- Indexes for table `pemesanan_produk`
+--
+ALTER TABLE `pemesanan_produk`
+  ADD PRIMARY KEY (`id_pemesanan_produk`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_menu`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  MODIFY `id_pemesanan` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+
+--
+-- AUTO_INCREMENT for table `pemesanan_produk`
+--
+ALTER TABLE `pemesanan_produk`
+  MODIFY `id_pemesanan_produk` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_menu` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
